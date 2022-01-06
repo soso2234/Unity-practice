fbd241066b4b0a164aa0d371f5fe9f87d17026af
soso2234 <mingi371@naver.com>
2022-01-06 13:29:10 +0900

    practice 1
    첫번째 중간커밋

---
+++ Jelly/Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+     public Vector3[] PointList;
+
+     public RuntimeAnimatorController[] LevelAc;
+
+     public void ChangeAc(Animator anim, int level){
+         anim.runtimeAnimatorController = LevelAc[level - 1];
+     }
+}
